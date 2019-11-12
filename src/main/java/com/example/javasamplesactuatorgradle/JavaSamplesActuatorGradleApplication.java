4caef316ef82e2b3fd613d923cb227a1851bd053
Matthew McNew <mmcnew@pivotal.io>
2019-11-12 12:20:28 -0600

    inital actuator commit

---
+++ src/main/java/com/example/javasamplesactuatorgradle/JavaSamplesActuatorGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javasamplesactuatorgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSamplesActuatorGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSamplesActuatorGradleApplication.class, args);
+	}
+
+}
