7ec7340040e610f4d856125eaeb9da2322c20fe2
matthewmcnew <mmcnew@pivotal.io>
2019-11-12 11:55:44 -0600

    initial commit

---
+++ src/main/java/com/example/buildjavasamples/BuildJavaSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.buildjavasamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildJavaSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildJavaSamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/buildjavasamples/BuildJavaSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildjavasamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildJavaSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
